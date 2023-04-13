46de6c22bc3360401b1d789d69db9e48de882254
Daniel Jurma <d_jurma@yahoo.com>
2023-04-13 19:37:53 +0300

    SearchScreen added and Search Filter

--- Screens/SearchScreen.js
+++ Screens/SearchScreen.js
@@ -1,11 +1,71 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
 import React from "react";
+import { Input } from "@ui-kitten/components";
+import { SafeAreaView, Dimensions } from "react-native";
+import { TouchableOpacity } from "react-native";
+import { Keyboard } from "react-native";
+import { useState } from "react";
+import SearchFilter from "../components/SearchFilter";
+
+const windowWidth = Dimensions.get("window").width;
 
 const SearchScreen = () => {
+  const [searchText, setSearchText] = useState("");
+
+  const SearchIcon = () => {
+    return (
+      <Image
+        source={require("../assets/Images/components-images/SearchIcon.png")}
+        style={{ width: 21, height: 21 }}
+      />
+    );
+  };
+
+  const CloseIcon = () => {
+    return (
+      <TouchableOpacity
+        onPress={() => {
+          Keyboard.dismiss();
+          setSearchText("");
+        }}
+      >
+        <Image
+          source={require("../assets/Images/components-images/CloseIcon.png")}
+          style={{ width: 19, height: 19, marginRight: 5 }}
+        />
+      </TouchableOpacity>
+    );
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>SearchScreen</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      {/* The searchbar  */}
+      <View
+        style={{
+          borderWidth: 0,
+          marginTop: 20,
+          width: windowWidth - 50,
+          height: 55,
+          alignSelf: "center",
+          justifyContent: "center",
+          backgroundColor: "#F4F4F4",
+          borderRadius: 6,
+          borderWidth: 1,
+          borderColor: "#BABABA",
+        }}
+      >
+        <Input
+          value={searchText}
+          onChangeText={(text) => setSearchText(text)}
+          placeholder="Search"
+          accessoryLeft={SearchIcon}
+          accessoryRight={CloseIcon}
+          style={{ backgroundColor: "transparent", borderWidth: 0 }}
+        />
+      </View>
+      {/* SearchFilter */}
+      <SearchFilter input={searchText}/>
+    </SafeAreaView>
   );
 };
 
@@ -14,8 +74,6 @@ export default SearchScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
     backgroundColor: "white",
   },
 });
---
+++ components/SearchFilter.js
@@ -0,0 +1,108 @@
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+import React from "react";
+import { data } from "../AC-IS-Data";
+
+const SearchFilter = ({ input }) => {
+  const materiiData = data.NUME_MATERII_ANUL2_AC_IS.MATERII;
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={materiiData}
+        renderItem={({ item }) => {
+          /* If the user didn't put any input in the search bar we are not gonna filter the list, and display all the data */
+          if (input === "")
+            return (
+              <View
+                style={{
+                  marginVertical: 25,
+                  justifyContent: "flex-start",
+                  alignItems: "center",
+                  flexDirection: "row",
+                }}
+              >
+                <Image
+                  source={item.MaterieIcon}
+                  style={{ width: 64, height: 64 }}
+                />
+                <Text
+                  style={{
+                    marginLeft: 15,
+                    fontSize: 20,
+                    width: 200,
+                    fontWeight: "300",
+                  }}
+                >
+                  {item.Nume}
+                </Text>
+                <TouchableOpacity style={{ height: 40 }}>
+                  <Image
+                    source={require("../assets/Images/components-images/RightArrowIcon.png")}
+                    style={{
+                      height: 20,
+                      width: 20,
+                      marginLeft: 20,
+                    }}
+                  />
+                </TouchableOpacity>
+              </View>
+            );
+          else if (item.Nume.toLowerCase().includes(input.toLowerCase())) {
+            return (
+              <View
+                style={{
+                  marginVertical: 25,
+                  justifyContent: "flex-start",
+                  alignItems: "center",
+                  flexDirection: "row",
+                }}
+              >
+                <Image
+                  source={item.MaterieIcon}
+                  style={{ width: 64, height: 64 }}
+                />
+                <Text
+                  style={{
+                    marginLeft: 15,
+                    fontSize: 20,
+                    width: 200,
+                    fontWeight: "300",
+                  }}
+                >
+                  {item.Nume}
+                </Text>
+                <TouchableOpacity style={{ height: 40 }}>
+                  <Image
+                    source={require("../assets/Images/components-images/RightArrowIcon.png")}
+                    style={{
+                      height: 20,
+                      width: 20,
+                      marginLeft: 20,
+                    }}
+                  />
+                </TouchableOpacity>
+              </View>
+            );
+          }
+        }}
+      />
+    </View>
+  );
+};
+
+export default SearchFilter;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 20,
+    marginLeft: 30,
+  },
+});
